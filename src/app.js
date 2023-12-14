fe7c90cced79659593820f2e0ecdd2d96b106422
Mayank <mayankghz225@gmail.com>
2023-12-14 15:51:02 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -10,22 +10,25 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(
   express.json({
     limit: "16kb",
   })
 );
-
 app.use(
   urlencoded({
     extended: true,
     limit: "16kb",
   })
 );
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes decleration
+app.use("/api/v1/users", userRouter);
+// http://localhost:8000/api/v1/users/register
 
 export { app };
